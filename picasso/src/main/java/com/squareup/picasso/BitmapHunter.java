ddd5b96bbdb60e0889dd210b38d7caefa52628a5
hjow <howsun.jow@gmail.com>
2015-05-19 00:24:14 +0100

    EXIF, centerCrop and centerInside fixes (see issues 1020,995,989)

--- picasso/src/main/java/com/squareup/picasso/BitmapHunter.java
+++ picasso/src/main/java/com/squareup/picasso/BitmapHunter.java
@@ -28,6 +28,14 @@ import java.util.List;
 import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import static android.media.ExifInterface.ORIENTATION_ROTATE_180;
+import static android.media.ExifInterface.ORIENTATION_ROTATE_270;
+import static android.media.ExifInterface.ORIENTATION_ROTATE_90;
+import static android.media.ExifInterface.ORIENTATION_FLIP_HORIZONTAL;
+import static android.media.ExifInterface.ORIENTATION_FLIP_VERTICAL;
+import static android.media.ExifInterface.ORIENTATION_TRANSPOSE;
+import static android.media.ExifInterface.ORIENTATION_TRANSVERSE;
+
 import static com.squareup.picasso.MemoryPolicy.shouldReadFromMemoryCache;
 import static com.squareup.picasso.Picasso.LoadedFrom.MEMORY;
 import static com.squareup.picasso.Picasso.Priority;
@@ -485,7 +493,7 @@ class BitmapHunter implements Runnable {
     return result;
   }
 
-  static Bitmap transformResult(Request data, Bitmap result, int exifRotation) {
+  static Bitmap transformResult(Request data, Bitmap result, int exifOrientation) {
     int inWidth = result.getWidth();
     int inHeight = result.getHeight();
     boolean onlyScaleDown = data.onlyScaleDown;
@@ -497,7 +505,7 @@ class BitmapHunter implements Runnable {
 
     Matrix matrix = new Matrix();
 
-    if (data.needsMatrixTransform()) {
+    if (data.needsMatrixTransform() || exifOrientation != 0) {
       int targetWidth = data.targetWidth;
       int targetHeight = data.targetHeight;
 
@@ -508,11 +516,38 @@ class BitmapHunter implements Runnable {
         } else {
           matrix.setRotate(targetRotation);
         }
+	// Recalulate dimensions after rotation
+        double tmpHeight = (double)targetHeight;
+        double tmpWidth  = (double)targetWidth;
+	double cosRotation = Math.cos(Math.toRadians(targetRotation));
+	double sinRotation = Math.sin(Math.toRadians(targetRotation));
+        targetHeight = (int)Math.floor((tmpHeight * cosRotation) + (tmpWidth * sinRotation)) ;
+        targetWidth  = (int)Math.floor((tmpHeight * sinRotation) + (tmpWidth * cosRotation)) ;
+      }
+
+      if (exifOrientation != 0) {
+        int exifRotation = getExifRotation(exifOrientation);
+        int exifTranslation = getExifTranslation(exifOrientation);
+	if (exifRotation != 0) {
+	  matrix.preRotate(exifRotation);
+	  if (exifRotation == 90 || exifRotation == 270) {
+             // Recalculate dimensions after rotation 
+             int tmpHeight = targetHeight;
+             targetHeight = targetWidth;
+             targetWidth = tmpHeight;
+          }
+        }
+        if (exifTranslation != 1) {
+          matrix.postScale(exifTranslation, 1);
+        }
       }
 
       if (data.centerCrop) {
-        float widthRatio = targetWidth / (float) inWidth;
-        float heightRatio = targetHeight / (float) inHeight;
+        // Keep aspect ratio if one dimension is set to 0
+        float widthRatio =
+            targetWidth != 0 ? targetWidth / (float) inWidth : targetHeight / (float) inHeight;
+        float heightRatio =
+            targetHeight != 0 ? targetHeight / (float) inHeight : targetWidth / (float) inWidth;
         float scaleX, scaleY;
         if (widthRatio > heightRatio) {
           int newSize = (int) Math.ceil(inHeight * (heightRatio / widthRatio));
@@ -520,19 +555,26 @@ class BitmapHunter implements Runnable {
           drawHeight = newSize;
           scaleX = widthRatio;
           scaleY = targetHeight / (float) drawHeight;
-        } else {
+        } else if (widthRatio < heightRatio) {
           int newSize = (int) Math.ceil(inWidth * (widthRatio / heightRatio));
           drawX = (inWidth - newSize) / 2;
           drawWidth = newSize;
           scaleX = targetWidth / (float) drawWidth;
           scaleY = heightRatio;
+        } else {
+          drawX = 0;
+          drawWidth = inWidth;
+          scaleX = scaleY = heightRatio;
         }
         if (shouldResize(onlyScaleDown, inWidth, inHeight, targetWidth, targetHeight)) {
           matrix.preScale(scaleX, scaleY);
         }
       } else if (data.centerInside) {
-        float widthRatio = targetWidth / (float) inWidth;
-        float heightRatio = targetHeight / (float) inHeight;
+        // Keep aspect ratio if one dimension is set to 0
+        float widthRatio =
+            targetWidth != 0 ? targetWidth / (float) inWidth : targetHeight / (float) inHeight;
+        float heightRatio =
+            targetHeight != 0 ? targetHeight / (float) inHeight : targetWidth / (float) inWidth;
         float scale = widthRatio < heightRatio ? widthRatio : heightRatio;
         if (shouldResize(onlyScaleDown, inWidth, inHeight, targetWidth, targetHeight)) {
           matrix.preScale(scale, scale);
@@ -552,10 +594,6 @@ class BitmapHunter implements Runnable {
       }
     }
 
-    if (exifRotation != 0) {
-      matrix.preRotate(exifRotation);
-    }
-
     Bitmap newResult =
         Bitmap.createBitmap(result, drawX, drawY, drawWidth, drawHeight, matrix, true);
     if (newResult != result) {
@@ -569,5 +607,41 @@ class BitmapHunter implements Runnable {
   private static boolean shouldResize(boolean onlyScaleDown, int inWidth, int inHeight,
       int targetWidth, int targetHeight) {
     return !onlyScaleDown || inWidth > targetWidth || inHeight > targetHeight;
+  }
+
+  static int getExifRotation(int orientation) {
+    int rotation;
+    switch (orientation) {
+      case ORIENTATION_ROTATE_90:
+      case ORIENTATION_TRANSPOSE:
+        rotation = 90;
+        break;
+      case ORIENTATION_ROTATE_180:
+      case ORIENTATION_FLIP_VERTICAL:
+        rotation = 180;
+        break;
+      case ORIENTATION_ROTATE_270:
+      case ORIENTATION_TRANSVERSE:
+        rotation = 270;
+        break;
+      default:
+        rotation = 0;
+    }
+    return rotation;
+  }
+
+ static int getExifTranslation(int orientation)  {
+    int translation;
+    switch (orientation) {
+      case ORIENTATION_FLIP_HORIZONTAL:
+      case ORIENTATION_FLIP_VERTICAL:
+      case ORIENTATION_TRANSPOSE:
+      case ORIENTATION_TRANSVERSE:
+        translation = -1;
+        break;
+      default:
+        translation = 1;
+    }
+    return translation;
   }
 }
